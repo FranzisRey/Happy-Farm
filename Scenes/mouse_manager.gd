c3e99a756968cff93fd21bcbf6f1ecc4b855d274
Franzis Rey <21-54198@g.batstate-u.edu.ph>
2026-05-15 18:13:57 +0800

    You can cut the godot bot now(will be used in the wheat minigame)

--- Scenes/mouse_manager.gd
+++ Scenes/mouse_manager.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
-var CutPoints = []
+var TrailPoints = []
+var ifMouseIsDown = false
+var CutPoints = [Vector2(NAN,NAN),Vector2(NAN,NAN)]
+var cutAction = false
+var mouseHeld = false
+
+@onready var cut_timer = $"../CutTimer"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,9 +14,33 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	CutPoints.push_front(get_global_mouse_position())
-	if CutPoints.size() >= 11:
-		CutPoints.pop_back()
-	print(CutPoints)
-	$Line2D.points = CutPoints
+	
+	if(Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+		
+		if(!cutAction && !mouseHeld):
+			CutPoints[0] = get_global_mouse_position()
+			cut_timer.start()
+		cutAction = true
+		
+		if(cut_timer.is_stopped()):
+			if cutAction && is_nan(CutPoints[1][0]):
+				CutPoints[1] = get_global_mouse_position()
+			cutAction = false
+		mouseHeld = true
+	else:
+		CutPoints = [Vector2(NAN,NAN),Vector2(NAN,NAN)]
+		cut_timer.stop()
+		cutAction = false
+		mouseHeld = false
+		
+	if(cutAction):
+		$Line.default_color = Color.RED
+	else:
+		$Line.default_color = Color.WHITE
+	
+	
+	TrailPoints.push_front(get_global_mouse_position())
+	if TrailPoints.size() >= 11:
+		TrailPoints.pop_back()
+	$Line.points = TrailPoints
 	pass
---
+++ Scenes/wheat_cut.gd
@@ -0,0 +1,27 @@
+extends Sprite2D
+
+var GotCut = false
+
+@export var mouse_manager: Node2D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	material.set("shader_parameter/offset", texture.get_size().y/4)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var CutPoints = mouse_manager.CutPoints
+	if !is_nan(CutPoints[1][0]) && !GotCut:
+		var minV = CutPoints[0].min(CutPoints[1])
+		var maxV = CutPoints[0].max(CutPoints[1])
+		if minV.x <= global_position.x-(texture.get_size().x/2) && global_position.x+(texture.get_size().x/2) <= maxV.x:
+			var slope = -tan(CutPoints[0].angle_to_point(CutPoints[1]))
+			var offset = ((minV.y+maxV.y)/2)
+			print(offset)
+			material.set("shader_parameter/offset", -offset+global_position.x+texture.get_size().x/2)
+			material.set("shader_parameter/slope", slope)
+			GotCut = true
+	elif is_nan(CutPoints[1][0]):
+		GotCut = false
+	pass
